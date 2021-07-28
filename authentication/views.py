f0f433c0c133d96606827f62b4077c35ceb376ed
Stevvvvvi <lly363893886@gmail.com>
2021-07-29 01:21:14 +1000

    asdf

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+
+class RegisterAPIView(GenericAPIView):
+
+    def post(self,request):
+        pass
 
 # Create your views here.
